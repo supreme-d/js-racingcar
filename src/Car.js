75c36697188ef26ee026f6ca4f3ddfabf18a4ede
supreme-d <dmhuh.data@gmail.com>
2024-04-27 22:24:42 +0900

    feat: Add position property, getPosition, and play method to Car class

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,20 @@
 export class Car {
-  constructor(name) {
+  constructor(name, position = 0) {
     this.name = name;
+    this.position = position;
   }
 
   getName() {
     return this.name;
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  play(value) {
+    if (value >= 4) {
+      this.position++;
+    }
   }
 }
