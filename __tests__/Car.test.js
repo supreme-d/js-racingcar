ee9a160f306b3982176e5f605dadafddea6a36e7
supreme-d <dmhuh.data@gmail.com>
2024-04-27 22:23:17 +0900

    test: Add tests to verify Car position logic with input thresholds

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -8,4 +8,28 @@ describe("Car", () => {
 
     expect(car.getName()).toBe(name);
   });
+
+  it("increase position by 1 if the given value is greater than or equal to 4", () => {
+    const name = "pobi";
+    const value = 5;
+    const car = new Car(name);
+
+    expect(car.getPosition()).toBe(0);
+
+    car.play(value);
+
+    expect(car.getPosition()).toBe(1);
+  });
+
+  it("should remain in the same position when the given value is less than 4", () => {
+    const name = "pobi";
+    const value = 1;
+    const car = new Car(name);
+
+    expect(car.getPosition()).toBe(0);
+
+    car.play(value);
+
+    expect(car.getPosition()).toBe(0);
+  });
 });
